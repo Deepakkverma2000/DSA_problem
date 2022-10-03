5cff2c920d2d4239c7c5456c08293aab5b71b248
Om Prakash <114151590+itsokop@users.noreply.github.com>
2022-10-03 18:42:00 +0530

    Update Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -3,6 +3,12 @@ using namespace std;
 #define ll long long
 
 int main() {
-	cout << "Hello World";
+	// to print hello world n times
+	int n;
+	cin>>n;
+	for(int i=1;i<n+1;i++){
+		cout << "Hello World"<<endl;
+	}
+	
 	return 0;
 }
